dbd10ba04e24c3ef24508fa381b2e96ab6207b3c
tjrwns1007@naver.com <tjrwns1007@naver.com>
2023-09-05 10:26:52 +0900

    코드 모듈화

---
+++ front/src/component/Main.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../css/main.css";
+import Navbar from "./navbar";
+
+const MainPage = () => {
+  return (
+    <header>
+      <body className="background">
+        <div className="background-image">
+          <div className="boxBlack">
+            <div className="boxW">
+              <p style={{ fontSize: "64px" }} className="textStyleW">
+                (주)함박오이{" "}
+              </p>
+              <p className="textStyleY">Safety first Smart Factory</p>
+              <Navbar />
+            </div>
+          </div>
+        </div>
+      </body>
+    </header>
+  );
+};
+
+export default MainPage;
