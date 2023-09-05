ea7ba49925c2f5f28b91ad95e2194ddb659c8db6
tjrwns1007@naver.com <tjrwns1007@naver.com>
2023-09-05 11:14:23 +0900

    add header

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Navigate, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import "./css/App.css";
 import Login from "./component/login";
 import Logout from "./component/Logout";
@@ -8,30 +8,23 @@ import HomePage from "./component/Home";
 import MainPage from "./component/Main";
 import KakaoOauth from "./component/kakao";
 import WebGL from "./component/webgl";
-
-// Navbar import
-// Logo 컴포넌트는 이제 Navbar 내에서 사용됩니다.
+import Header from "./component/Header";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <header className="App-header">
-          <Link to={"/"}>Home</Link>
-          <Link to={"/webgl"}>3D MODEL</Link>
-        </header>
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Navigate to="/main" />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/Logout" element={<Logout />} />
-            <Route path="/webgl" element={<WebGL />} />
-            <Route path="/chart" element={<GraphComponent />} />
-            <Route path="/main" element={<MainPage />} />
-            <Route path="/kakao" element={<KakaoOauth />} />
-            <Route path="/homePage" element={<HomePage />} />
-          </Routes>
-        </div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Navigate to="/main" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Logout" element={<Logout />} />
+          <Route path="/webgl" element={<WebGL />} />
+          <Route path="/chart" element={<GraphComponent />} />
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/kakao" element={<KakaoOauth />} />
+          <Route path="/homePage" element={<HomePage />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
---
+++ front/src/component/Header.js
@@ -0,0 +1,46 @@
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Tabs,
+  Tab,
+  CssBaseline,
+} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Airplay from "@mui/icons-material/Airplay";
+
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div>
+      <CssBaseline />
+      <AppBar
+        position="absolute"
+        style={{ backgroundColor: "transparent", boxShadow: "none" }}
+      >
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}></Typography>
+          <Tabs>
+            <Tab label="Home" icon={<HomeIcon />} component={Link} to="/main" />
+            <Tab
+              label="DashBoard"
+              icon={<Airplay />}
+              component={Link}
+              to="/webgl"
+            />
+            <Tab
+              label="Profile"
+              icon={<AccountCircleIcon />}
+              component={Link}
+              to="/profile"
+            />
+          </Tabs>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default Header;
