705ae7a430a8c6f1bc8b907a0c5af292c627c616
JuYeung2 <bcd328@naver.com>
2023-09-06 13:38:41 +0900

    경로 오류 수정

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import GraphComponent from "./component/Chart/Graph";
-import Dash from "./component/Chart/dash";
+import GraphComponent from './Chart/Graph';
+import Dash from "./Chart/dash";
 import KakaoOauth from "./component/user/kakao";
 
-import Header from "./component/Header/Header";
+import Header from "./Header/Header";
 import HomePage from "./component/HomePage";
 import '../src/css/App.css';
 
