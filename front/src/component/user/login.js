553bcf414dcc0ecda2000b33599889e08598c11e
tjrwns1007@naver.com <tjrwns1007@naver.com>
2023-09-15 18:23:12 +0900

    update file

--- front/src/component/user/login.js
+++ front/src/component/user/login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../userCss/loginPage.css";
+import "../css/loginPage.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
