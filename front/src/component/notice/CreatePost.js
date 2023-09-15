320eef71e9acc9a7c40f1d9a3cb29861def8e43f
tjrwns1007@naver.com <tjrwns1007@naver.com>
2023-09-15 17:46:09 +0900

    add css

--- front/src/component/notice/CreatePost.js
+++ front/src/component/notice/CreatePost.js
@@ -1,50 +1,56 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import '../../css/CreatePost.css'
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import "../../css/CreatePost.css";
+import { useNavigate } from "react-router-dom";
+
 const CreatePostPage = () => {
   const navigate = useNavigate();
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
 
-      
-    axios.post(`${process.env.REACT_APP_URL}/notice/notify`, 
-    { // 요청 본문
-      title: title,
-      content: content,
-    },
-    { // 설정 객체
-      headers: { Authorization: `${localStorage.getItem("token")}` },
-    }
-  )
-  .then(response => {
-    console.log('게시글이 성공적으로 등록되었습니다.');
-    
-    // 게시글 등록 후 /notice로 이동합니다.
-    window.location.reload(navigate("/"));
+    axios
+      .post(
+        `${process.env.REACT_APP_URL}/notice/notify`,
+        {
+          title: title,
+          content: content,
+        },
+        {
+          headers: { Authorization: `${localStorage.getItem("token")}` },
+        }
+      )
+      .then((response) => {
+        console.log("게시글이 성공적으로 등록되었습니다.");
+        window.location.reload(navigate("/"));
       })
-      .catch(error => console.error('Error:', error));
-    
-    setTitle('');
-    setContent('');
-};
+      .catch((error) => console.error("Error:", error));
 
-return (
-  <form onSubmit={handleFormSubmit} className="post-form">
-    <label className="post-label" style={{ marginTop: '100px' }}>
-      제목:
-      <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-    </label>
-    <label className="post-label" style={{ marginTop: '100px' }}>
-      내용:
-      <textarea value={content} onChange={(e) => setContent(e.target.value)} />
-    </label>
-    <button type="submit">등록</button>
-  </form>
-);
+    setTitle("");
+    setContent("");
+  };
+
+  return (
+    <div className="post" style={{ backgroundAttachment: "fixed" }}>
+      <form onSubmit={handleFormSubmit}>
+        <label className="post-label-1">
+          제목
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </label>
+        <label className="post-label-2">
+          내용
+          <input value={content} onChange={(e) => setContent(e.target.value)} />
+        </label>
+        <button type="submit">등록하기</button>
+      </form>
+    </div>
+  );
 };
 
 export default CreatePostPage;
