6e6c67e3649bb5c1a5915485eaf7ac3d49a92603
gusdn98 <ah529@naver.com>
2023-09-15 10:47:08 +0900

    fix:PostView.js댓글 달기 추가

--- front/src/component/notice/NoticeBoard.js
+++ front/src/component/notice/NoticeBoard.js
@@ -1,10 +1,11 @@
 // Notice.js
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import CommonTable from './CommonTable';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CommonTable from "./CommonTable";
+import { useNavigate } from "react-router-dom";
+import "../../css/Post.css";
 const NoticeBoard = () => {
-  const headersName = ['글번호', '제목', '공지날짜', '조회수'];
+  const headersName = ["글번호", "제목", "공지날짜", "조회수"];
   const [data, setData] = useState([]);
 
   // useNavigate 훅을 사용하여 navigate 함수를 가져옵니다.
@@ -30,34 +31,37 @@ const NoticeBoard = () => {
 
         setData(formattedData);
       })
-      .catch((error) => console.error('Error:', error));
+      .catch((error) => console.error("Error:", error));
   }, []);
 
-// 각 row가 클릭되면 해당 게시물의 상세 페이지로 네비게이션하는 핸들러
-const handleRowClick = (noticeId) => {
+  // 각 row가 클릭되면 해당 게시물의 상세 페이지로 네비게이션하는 핸들러
+  const handleRowClick = (noticeId) => {
     navigate(`/postView/${noticeId}`);
-};
+  };
 
-const handleCreatePostButtonClick = () => {
-  navigate('/create-post');
-};
+  const handleCreatePostButtonClick = () => {
+    navigate("/create-post");
+  };
 
+  return (
+    <div className="post-back-ground ">
+      <div style={{ paddingTop: "73px", height: "100vh", width: "100vw" }}>
+        <div style={{ textAlign: "center", margin: "20px 0" }}>
+          <h1>공지사항</h1>
+        </div>
 
-return (
-  <div style={{ paddingTop: "73px", height: "100vh", width: "100vw" }}>
-    <div style={{ textAlign: 'center', margin: '20px 0' }}>
-      <h1>공지사항</h1>
+        {/* headersName과 data를 전달하고 내부에서 thead와 tbody를 관리하도록 함 */}
+        <CommonTable headersName={headersName} data={data} />
+
+        <button
+          onClick={handleCreatePostButtonClick}
+          style={{ position: "fixed", left: 0, bottom: 0 }}
+        >
+          게시물 등록
+        </button>
+      </div>
     </div>
+  );
+};
 
-    {/* headersName과 data를 전달하고 내부에서 thead와 tbody를 관리하도록 함 */}
-    <CommonTable headersName={headersName} data={data} />
-    
-
-    <button onClick={handleCreatePostButtonClick} style={{ position: 'fixed', left: 0, bottom: 0 }}>
-      게시물 등록
-    </button>
-  </div>  
-);
-}
-
-export default NoticeBoard
+export default NoticeBoard;
