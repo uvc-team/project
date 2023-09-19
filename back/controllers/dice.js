b6463c8bc2efe0cdb8bfd47dbe751d2c4cf9f71d
tjrwns1007@naver.com <tjrwns1007@naver.com>
2023-09-19 11:23:36 +0900

    add conveyor state

--- back/controllers/dice.js
+++ back/controllers/dice.js
@@ -1,10 +1,12 @@
+const DiceData = require("../models/dices");
+
 // MQTT 데이터를 조회하는 컨트롤러
 exports.dataget = async (req, res) => {
   try {
-    const dicedata = await MqttData.aggregate([
-      { $unwind: "$Number" },
+    const dicedata = await DiceData.aggregate([
+      { $unwind: "$DiceNumber" },
       { $sort: { _id: -1 } },
-    ]).limit(1);
+    ]).limit(30);
     if (dicedata) {
       res.status(200).json(dicedata);
     } else {
